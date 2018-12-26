1f84bdcdb440ff802b5bf39a27b58f4237909b1d
박세은 <pkse1213@naver.com>
2018-12-26 14:03:16 +0900

    랭킹 리스트 뷰 완료

---
+++ Moca-iOS/Viewcontrollers/HomeTab/HomeRankingListVC.swift
@@ -0,0 +1,46 @@
+//
+//  HomeRankingListVC.swift
+//  Moca-iOS
+//
+//  Created by 박세은 on 2018. 12. 26..
+//  Copyright © 2018년 박세은. All rights reserved.
+//
+
+import UIKit
+
+class HomeRankingListVC: UIViewController {
+
+    @IBOutlet weak var rankingCollectionView: UICollectionView!
+    var unit: CGFloat = 0
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpCollectionView()
+    }
+    
+    private func setUpCollectionView() {
+        self.rankingCollectionView.delegate = self
+        self.rankingCollectionView.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        unit = (self.rankingCollectionView.frame.width/2-3)/175
+        print(unit)
+    }
+}
+
+extension HomeRankingListVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width:175*unit , height: 238*unit)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = rankingCollectionView.dequeueReusableCell(withReuseIdentifier: "RankingCafeCell", for: indexPath) as! RankingCafeCell
+        return cell
+    }
+    
+}
