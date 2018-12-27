e0be41c020cf947c7d299a336710c962085cc838
박세은 <pkse1213@naver.com>
2018-12-27 20:06:47 +0900

    이미지 사이즈 변경

--- Moca-iOS/Viewcontrollers/HomeTab/MocaPicksListVC.swift
+++ Moca-iOS/Viewcontrollers/HomeTab/MocaPicksListVC.swift
@@ -35,5 +35,8 @@ extension MocaPicksListVC: UITableViewDelegate, UITableViewDataSource {
         
     }
     
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = UIStoryboard(name: "HomeTab", bundle: nil).instantiateViewController(withIdentifier: "MocaPicksCafeVC") as! MocaPicksCafeVC
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
 }
