eb31baed002d21be33f8c947a3918998c26119bf
박세은 <pkse1213@naver.com>
2018-12-27 15:30:51 +0900

    moca picks 이미지 스크롤뷰 progress bar

--- Moca-iOS/Viewcontrollers/HomeTab/MocaPicksCafeVC.swift
+++ Moca-iOS/Viewcontrollers/HomeTab/MocaPicksCafeVC.swift
@@ -10,8 +10,11 @@ import UIKit
 
 class MocaPicksCafeVC: UIViewController {
 
+    let colors = [#colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1),#colorLiteral(red: 0.4392156899, green: 0.01176470611, blue: 0.1921568662, alpha: 1),#colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1),#colorLiteral(red: 0.7254902124, green: 0.4784313738, blue: 0.09803921729, alpha: 1),#colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1)]
     @IBOutlet weak var cafeImageCollectionView: UICollectionView!
     @IBOutlet weak var baristaTableView: UITableView!
+    @IBOutlet weak var scrollProgressView: UIProgressView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpListView()
@@ -24,7 +27,10 @@ class MocaPicksCafeVC: UIViewController {
         baristaTableView.delegate = self
         baristaTableView.dataSource = self
         baristaTableView.applyRadius(radius: 10)
+        
     }
+    
+    
     
 }
 
@@ -43,14 +49,18 @@ extension MocaPicksCafeVC: UITableViewDelegate, UITableViewDataSource {
 }
 
 extension MocaPicksCafeVC: UICollectionViewDelegate, UICollectionViewDataSource {
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 5
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = cafeImageCollectionView.dequeueReusableCell(withReuseIdentifier: "CommunityContentImageCell", for: indexPath) as! CommunityContentImageCell
+        cell.backgroundColor = colors[indexPath.item]
         return cell
         
     }
-    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+       
+    }
 }
