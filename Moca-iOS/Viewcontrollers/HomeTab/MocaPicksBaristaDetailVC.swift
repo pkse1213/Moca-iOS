640770331f864584eb83a31a0e4ed77af0ebb9a7
박세은 <pkse1213@naver.com>
2018-12-28 17:42:44 +0900

    <Moca Picks barista Detail View> 뷰 틀 구성

---
+++ Moca-iOS/Viewcontrollers/HomeTab/MocaPicksBaristaDetailVC.swift
@@ -0,0 +1,39 @@
+//
+//  MocaPicksDetailVC.swift
+//  Moca-iOS
+//
+//  Created by 박세은 on 2018. 12. 28..
+//  Copyright © 2018년 박세은. All rights reserved.
+//
+
+import UIKit
+
+class MocaPicksBaristaDetailVC: UIViewController {
+
+    @IBOutlet weak var baristaDetailTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpTableView()
+    }
+    
+    private func setUpTableView() {
+        baristaDetailTableView.delegate = self
+        baristaDetailTableView.dataSource = self
+    }
+}
+
+extension MocaPicksBaristaDetailVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        if let baristaCell = baristaDetailTableView.dequeueReusableCell(withIdentifier: "MocaPicksBaristaDetailCell") as? MocaPicksBaristaDetailCell {
+            cell = baristaCell
+        }
+        return cell
+    }
+    
+}
---
+++ Moca-iOS/Views/HomeTab/MocaPicks/MocaPicksBaristaDetailCell.swift
@@ -0,0 +1,28 @@
+//
+//  MocaPicksDetailCell.swift
+//  Moca-iOS
+//
+//  Created by 박세은 on 2018. 12. 28..
+//  Copyright © 2018년 박세은. All rights reserved.
+//
+
+import UIKit
+
+class MocaPicksBaristaDetailCell: UITableViewCell {
+
+    @IBOutlet weak var profileBackgroundView: UIView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    private func setUpView() {
+        
+    }
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
