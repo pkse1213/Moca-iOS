ffb28c8fa12917586c8e71e67f96d414eea0e8e9
박세은 <pkse1213@naver.com>
2018-12-28 17:49:07 +0900

    <Moca Picks barista Detail View> 뷰 완료

--- Moca-iOS/Views/HomeTab/MocaPicks/MocaPicksBaristaDetailCell.swift
+++ Moca-iOS/Views/HomeTab/MocaPicks/MocaPicksBaristaDetailCell.swift
@@ -9,16 +9,25 @@
 import UIKit
 
 class MocaPicksBaristaDetailCell: UITableViewCell {
-
     @IBOutlet weak var profileBackgroundView: UIView!
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var baristaNameLabel: UILabel!
+    @IBOutlet weak var evaluationNameLabel: UILabel!
+    @IBOutlet weak var evaluationScoreLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        
+        setUpView()
     }
     
     private func setUpView() {
+        profileBackgroundView.applyRadius(radius: 5)
+        profileBackgroundView.applyBorder(width: 0.5, color: #colorLiteral(red: 0.4392156863, green: 0.4392156863, blue: 0.4392156863, alpha: 1))
         
+        profileImageView.applyRadius(radius: 23)
     }
+    
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
