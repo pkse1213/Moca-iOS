4dc476009731881888ce65d12495767f2ce01fc2
조수민 <soomin@josumin-ui-MacBook-Pro.local>
2019-01-04 07:32:19 +0900

    <LoginSignUp.storyboard> 로그인 화면 구현

---
+++ Moca-iOS/Viewcontrollers/LoginSignUp/LoginVC.swift
@@ -0,0 +1,81 @@
+//
+//  LoginVC.swift
+//  Moca-iOS
+//
+//  Created by 조수민 on 04/01/2019.
+//  Copyright © 2019 박세은. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    @IBOutlet var loginBtn: UIButton!
+    @IBOutlet var idCircleImage: UIImageView!
+    @IBOutlet var pwCircleImage: UIImageView!
+    @IBOutlet var idTxt: UITextField!
+    @IBOutlet var pwTxt: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setUpTxtField()
+        setLoginBtn()
+    }
+    
+    // 텍스트필드 delegate
+    private func setUpTxtField() {
+        idTxt.delegate = self
+        pwTxt.delegate = self
+        
+        idCircleImage.isHidden = true
+        pwCircleImage.isHidden = true
+    }
+    
+    // 로그인 버튼 radius
+    private func setLoginBtn() {
+        loginBtn.layer.masksToBounds = false
+        loginBtn.layer.cornerRadius = 19.0
+        loginBtn.clipsToBounds = true
+    }
+    
+    // 로그인
+    @IBAction func loginAction(_ sender: Any) {
+        
+    }
+    
+    // 회원가입 화면으로
+    @IBAction func goToSignupAction(_ sender: Any) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "SignupVC") as? SignupVC {
+            present(vc, animated: true)
+        }
+    }
+}
+
+extension LoginVC : UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        print("end")
+        if textField == self.idTxt {
+            if textField.text != "" {
+                idCircleImage.isHidden = false
+            }
+            else {
+                idCircleImage.isHidden = true
+            }
+        }
+        else if textField == self.pwTxt {
+            if textField.text != "" {
+                pwCircleImage.isHidden = false
+            }
+            else {
+                pwCircleImage.isHidden = true
+            }
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return true
+    }
+}
---
+++ Moca-iOS/Viewcontrollers/LoginSignUp/SignupVC.swift
@@ -0,0 +1,20 @@
+//
+//  SignupVC.swift
+//  Moca-iOS
+//
+//  Created by 조수민 on 04/01/2019.
+//  Copyright © 2019 박세은. All rights reserved.
+//
+
+import UIKit
+
+class SignupVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
